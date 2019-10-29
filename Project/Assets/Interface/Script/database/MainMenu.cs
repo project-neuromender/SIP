3826a7a6668e21d3ec108dfeda3385f25aa11268
nuramiera <nuramiera@users.github.com>
2019-10-29 13:52:44 +0800

    Edit Main Menu

--- Project/Assets/Interface/Script/database/MainMenu.cs
+++ Project/Assets/Interface/Script/database/MainMenu.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using Valve.VR;
+using Valve.VR.Extras;
+using Valve.VR.InteractionSystem;
 
 public class MainMenu : MonoBehaviour
 {
@@ -12,17 +15,43 @@ public class MainMenu : MonoBehaviour
 
     public Text playerDisplay;
 
+    public SteamVR_LaserPointer laserPointer;
+
+    void Awake()
+    {
+        laserPointer.PointerClick += PointerClick;
+    }
+
     private void Start()
     {
-        if(DBManager.LoggedIn)
+        if (DBManager.LoggedIn)
         {
             playerDisplay.text = "Click Lobby to Start";
         }
-        
+
 
         registerButton.interactable = !DBManager.LoggedIn;
         loginButton.interactable = !DBManager.LoggedIn;
         playButton.interactable = DBManager.LoggedIn;
+    }
+
+    public void PointerClick(object sender, PointerEventArgs e)
+    {
+        if (e.target.name == "Register")
+        {
+            Debug.Log("Register was clicked");
+            GotoRegister();
+        }
+        else if (e.target.name == "Login")
+        {
+            Debug.Log("Login was clicked");
+            GotoLogin();
+        }
+        else if (e.target.name == "Lobby")
+        {
+            Debug.Log("Lobby was clicked");
+            GotoGame();
+        }
     }
 
     public void GotoRegister()
